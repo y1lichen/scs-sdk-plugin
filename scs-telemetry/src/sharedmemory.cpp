92abf4628571ee10c8ae6adf7fc7224652346b8f
y1lichen <f1jefferson2@gmail.com>
2025-07-19 02:06:19 +0800

    add build for macos

--- scs-telemetry/src/sharedmemory.cpp
+++ scs-telemetry/src/sharedmemory.cpp
@@ -1,24 +1,20 @@
+#include "sharedmemory.hpp"
+
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include<stdio.h>
-#include <string.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <errno.h>
-
-#include "sharedmemory.hpp"
+#include <string>
 
 void SharedMemory::LogError(const char *logPtr) {
-//    printf("%s", logPtr);
 #ifdef SHAREDMEM_LOGGING
-    if (this->logFilePtr == nullptr)
-    {
+    if (this->logFilePtr == nullptr) {
         this->logFilePtr = fopen(SHAREDMEM_FILENAME, "w");
     }
-    if (this->logFilePtr != nullptr)
-    {
+    if (this->logFilePtr != nullptr) {
         fprintf(this->logFilePtr, "%s\n", logPtr);
-//        fflush(this->logFilePtr);
     }
 #endif
     this->errReason = logPtr;
@@ -31,61 +27,72 @@ SharedMemory::SharedMemory(const char *newNamePtr, unsigned int size) {
 #ifdef SHAREDMEM_LOGGING
     this->logFilePtr = nullptr;
 #endif
+
+    // Platform-specific shm dir
+#ifdef __APPLE__
+    const char *shmDir = "/tmp/SCS";
+#else
+    const char *shmDir = "/dev/shm/SCS";
+#endif
+
     struct stat st = {0};
-    if (stat("/dev/shm/SCS", &st) == -1) {
-        LogError("Creating directory /dev/shm/SCS");
-        mkdir("/dev/shm/SCS", 0777); // rw-rw-rw
-    }else {
-        int perms = (S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-        if((st.st_mode & perms) != perms){
-//            system("rm -rf /dev/shm/SCS");
-            LogError("Changing permission of /dev/shm/SCS");
-            chmod("/dev/shm/SCS", 0777); // rw-rw-rw
+    if (stat(shmDir, &st) == -1) {
+        LogError("Creating shared memory directory");
+        mkdir(shmDir, 0777); // rwxrwxrwx
+    } else {
+        int perms = (S_IRWXU | S_IRWXG | S_IRWXO);  // rwxrwxrwx
+        if ((st.st_mode & perms) != perms) {
+            LogError("Changing permission of shared memory directory");
+            chmod(shmDir, 0777);
         }
     }
-    hMapFile = open(newNamePtr, O_CREAT | O_RDWR, 0777);
+
+    // Full file path for mmap file
+    std::string fullPath = std::string(shmDir) + "/" + newNamePtr;
+
+    hMapFile = open(fullPath.c_str(), O_CREAT | O_RDWR, 0777);
     if (hMapFile == -1) {
-        chmod(newNamePtr, 0777); // rw-rw-rw
-        hMapFile = open(newNamePtr, O_CREAT | O_RDWR, 0777);
-        if(hMapFile == -1){
-            printf("%s\n",strerror(errno));
-            LogError("Unable to open mmap file");
-            return;
-        }
-    }
-    this->pBufferPtr = mmap(nullptr, mapsize, PROT_READ | PROT_WRITE, MAP_SHARED, hMapFile, 0);
-    LogError("Created file map");
-    if (this->pBufferPtr == MAP_FAILED) {
-        LogError("Error calling mmap");
+        LogError("Unable to open mmap file");
         return;
     }
 
-    ftruncate(hMapFile, mapsize);
+    // Set file size before mmap
+    if (ftruncate(hMapFile, mapsize) == -1) {
+        LogError("ftruncate failed");
+        close(hMapFile);
+        return;
+    }
+
+    this->pBufferPtr = mmap(nullptr, mapsize, PROT_READ | PROT_WRITE, MAP_SHARED, hMapFile, 0);
+    if (this->pBufferPtr == MAP_FAILED) {
+        LogError("Error calling mmap");
+        close(hMapFile);
+        return;
+    }
+
     memset(this->pBufferPtr, 0, mapsize);
     this->isSharedMemoryHooked = true;
     LogError("Opened MMF!");
 }
 
-
 void SharedMemory::Close() {
 #ifdef SHAREDMEM_LOGGING
-    if (logFilePtr != nullptr)
-    {
+    if (logFilePtr != nullptr) {
         fflush(logFilePtr);
         fclose(logFilePtr);
+        logFilePtr = nullptr;
     }
 #endif
     if (isSharedMemoryHooked) {
         if (pBufferPtr != nullptr) {
-            munmap(pBufferPtr, this->mapsize);
+            munmap(pBufferPtr, mapsize);
             pBufferPtr = nullptr;
         }
-        if (hMapFile > 0) {
+        if (hMapFile >= 0) {
             close(hMapFile);
-            hMapFile = 0;
+            hMapFile = -1;
         }
     }
 
     isSharedMemoryHooked = false;
-
 }
